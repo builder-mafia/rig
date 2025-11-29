45044ba93403cfb163f171991c395b0ac3729d4f
gaki2 <qudrkr0815@gmail.com>
2025-11-29 23:11:08 +0900

    refactor: migrate to react-hook-forms

--- apps/web/src/app/components/modal/ApiKeyConfigModal.tsx
+++ apps/web/src/app/components/modal/ApiKeyConfigModal.tsx
@@ -1,14 +1,15 @@
-import { Google, OpenAI } from '@lobehub/icons';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { useSetAtom } from 'jotai';
 import { Loader2, Plus } from 'lucide-react';
 import { useLayoutEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { toast } from 'sonner';
+import z from 'zod';
 import { Button } from '@/components/ui/button';
 import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
@@ -20,11 +21,42 @@ import {
 } from '@/components/ui/dialog';
 import { Label } from '@/components/ui/label';
 import { PasswordInput } from '@/components/ui/password-input';
+import {
+  type LLMProviderName,
+  LLMProviderNameSchema,
+} from '@/core/provider/all-models';
 import { validateApiKey } from '@/core/provider/validate-apikey';
 import { DB } from '@/idb/db';
 import { dbAtoms } from '@/idb/dbStore';
+import { getProviderLogo } from '../helper/providerLogo';
 
-// need to refactor this component.
+const FormValuesSchema = z.object(
+  Object.fromEntries(
+    LLMProviderNameSchema.options.map(provider => [
+      provider,
+      z.string().optional(),
+    ]),
+  ) as Record<LLMProviderName, z.ZodOptional<z.ZodString>>,
+);
+
+type FormValuesType = z.infer<typeof FormValuesSchema>;
+
+type StatusMap = Record<
+  LLMProviderName,
+  {
+    isLoading: boolean;
+    isValid: boolean;
+  }
+>;
+
+const createInitialStatusMap = (): StatusMap =>
+  Object.fromEntries(
+    LLMProviderNameSchema.options.map(provider => [
+      provider,
+      { isLoading: false, isValid: false },
+    ]),
+  ) as StatusMap;
+
 export const ApiKeyConfigModal = ({
   open,
   onOpenChange,
@@ -32,117 +64,96 @@ export const ApiKeyConfigModal = ({
   open: boolean;
   onOpenChange: (open: boolean) => void;
 }) => {
-  const [openaiApiKey, setOpenaiApiKey] = useState('');
-  const [isOpenaiApiKeyValid, setIsOpenaiApiKeyValid] = useState(false);
-  const [isOpenaiApiKeyLoading, setIsOpenaiApiKeyLoading] = useState(true);
-  const [isOpenaiApiKeyFailed, setIsOpenaiApiKeyFailed] = useState(false);
+  const { register, handleSubmit, setError, formState, setValue, reset } =
+    useForm<FormValuesType>({
+      resolver: zodResolver(FormValuesSchema),
+      defaultValues: {},
+    });
 
-  const [googleApiKey, setGoogleApiKey] = useState('');
-  const [isGoogleApiKeyValid, setIsGoogleApiKeyValid] = useState(false);
-  const [isGoogleApiKeyLoading, setIsGoogleApiKeyLoading] = useState(true);
-  const [isGoogleApiKeyFailed, setIsGoogleApiKeyFailed] = useState(false);
+  const [statusMap, setStatusMap] = useState<StatusMap>(() =>
+    createInitialStatusMap(),
+  );
 
   const setConfig = useSetAtom(dbAtoms.configAtom);
 
-  const resetState = () => {
-    setIsOpenaiApiKeyValid(false);
-    setIsOpenaiApiKeyFailed(false);
-    setIsOpenaiApiKeyLoading(true);
-    setIsGoogleApiKeyValid(false);
-    setIsGoogleApiKeyFailed(false);
-    setIsGoogleApiKeyLoading(true);
-  };
-
   useLayoutEffect(() => {
     if (open) {
-      resetState();
+      // this modal is not unmounted. so we need to clear previous status.
+      reset();
 
-      const getApiKeys = async () => {
-        const { googleApiKey, openaiApiKey } = await DB.getConfig();
-        setGoogleApiKey(googleApiKey ?? '');
-        setOpenaiApiKey(openaiApiKey ?? '');
+      const setInitialValue = async () => {
+        const { apiKeys } = await DB.getConfig();
 
-        if (googleApiKey) {
-          const isValid = await validateApiKey({
-            apiKey: googleApiKey,
-            providerName: 'google',
-          });
-          setIsGoogleApiKeyValid(isValid);
-        }
-
-        if (openaiApiKey) {
-          const isValid = await validateApiKey({
-            apiKey: openaiApiKey,
-            providerName: 'openai',
-          });
-          setIsOpenaiApiKeyValid(isValid);
-        }
-
-        setIsOpenaiApiKeyLoading(false);
-        setIsGoogleApiKeyLoading(false);
+        LLMProviderNameSchema.options.forEach(providerName => {
+          const apiKey = apiKeys[providerName];
+          if (!apiKey) {
+            setValue(providerName, '');
+            return;
+          }
+          setValue(providerName, apiKey);
+        });
       };
 
-      getApiKeys();
+      void setInitialValue();
     }
-  }, [open]);
+  }, [open, setValue]);
 
-  const handleValidateAndSaveOpenAI = async () => {
-    setIsOpenaiApiKeyLoading(true);
-    setIsOpenaiApiKeyValid(false);
+  const handleValidateAndSave =
+    (providerName: LLMProviderName) => async (values: FormValuesType) => {
+      let isSuccessful = false;
 
-    const isValid = await validateApiKey({
-      apiKey: openaiApiKey,
-      providerName: 'openai',
-    });
-    if (isValid) {
-      setConfig({ openaiApiKey });
-      setIsOpenaiApiKeyValid(true);
-      toast.success('OpenAI API Key has been saved.', {
-        position: 'top-center',
-        duration: 2000,
-      });
-    } else {
-      setIsOpenaiApiKeyFailed(true);
-    }
+      setStatusMap(prev => ({
+        ...prev,
+        [providerName]: {
+          isLoading: true,
+          isValid: false,
+        },
+      }));
 
-    setIsOpenaiApiKeyLoading(false);
-  };
+      try {
+        const apiKey = values[providerName];
+        const error = new Error('invalid-api-key');
 
-  const handleValidateAndSaveGoogle = async () => {
-    setIsGoogleApiKeyLoading(true);
-    setIsGoogleApiKeyValid(false);
+        if (!apiKey) {
+          throw error;
+        }
 
-    const isValid = await validateApiKey({
-      apiKey: googleApiKey,
-      providerName: 'google',
-    });
-    if (isValid) {
-      setConfig({ googleApiKey });
-      setIsGoogleApiKeyValid(true);
-      toast.success('Google API Key has been saved.', {
-        position: 'top-center',
-        duration: 2000,
-      });
-    } else {
-      setIsGoogleApiKeyFailed(true);
-    }
+        const isValid = await validateApiKey({
+          apiKey,
+          providerName,
+        });
 
-    setIsGoogleApiKeyLoading(false);
-  };
+        if (!isValid) {
+          throw error;
+        }
 
-  const onChangeOpenaiApiKey = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setIsOpenaiApiKeyValid(false);
-    setIsOpenaiApiKeyFailed(false);
+        isSuccessful = true;
 
-    setOpenaiApiKey(e.target.value);
-  };
+        await setConfig({
+          apiKeys: {
+            [providerName]: apiKey,
+          },
+        });
 
-  const onChangeGoogleApiKey = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setIsGoogleApiKeyValid(false);
-    setIsGoogleApiKeyFailed(false);
-
-    setGoogleApiKey(e.target.value);
-  };
+        toast.success(`${providerName} API Key has been saved.`, {
+          position: 'top-center',
+          duration: 3000,
+        });
+      } catch (err) {
+        setError(providerName, {
+          message: 'Invalid API key. Please check your key and try again.',
+        });
+        return;
+      } finally {
+        setStatusMap(prev => ({
+          ...prev,
+          [providerName]: {
+            isLoading: false,
+            isValid: isSuccessful,
+          },
+        }));
+      }
+    };
 
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
@@ -165,98 +176,71 @@ export const ApiKeyConfigModal = ({
           <CardContent className='mt-2'>
             <div className='grid gap-6'>
               <div className='flex flex-col gap-2'>
-                <div className='flex flex-row gap-2'>
-                  <Label htmlFor='openai-api-key'>
-                    <OpenAI />
-                    OpenAI
-                  </Label>
-                  {isOpenaiApiKeyLoading && (
-                    <span className='text-yellow-500 text-xs'>
-                      Validating OpenAI API Key...
-                    </span>
-                  )}
-                  {!isOpenaiApiKeyLoading && isOpenaiApiKeyValid && (
-                    <span className='text-green-500 text-xs'>
-                      OpenAI API Key is valid.
-                    </span>
-                  )}
-                  {!isOpenaiApiKeyLoading && isOpenaiApiKeyFailed && (
-                    <span className='text-red-500 text-xs'>
-                      OpenAI API Key is invalid.
-                    </span>
-                  )}
-                </div>
-                <div className='flex gap-1'>
-                  <PasswordInput
-                    id='openai-api-key'
-                    placeholder=''
-                    value={openaiApiKey}
-                    onChange={onChangeOpenaiApiKey}
-                    showPassword={false}
-                    autoFocus={false}
-                  />
-                  <Button
-                    variant='ghost'
-                    size='icon'
-                    onClick={handleValidateAndSaveOpenAI}
-                    disabled={isOpenaiApiKeyLoading}
-                  >
-                    {isOpenaiApiKeyLoading ? (
-                      <Loader2 className='animate-spin' />
-                    ) : (
-                      <Plus />
-                    )}
-                  </Button>
-                </div>
-              </div>
-              <div className='flex flex-col gap-2'>
-                <div className='flex flex-row gap-2'>
-                  <Label htmlFor='google-api-key'>
-                    <Google />
-                    Google
-                  </Label>
-                  {isGoogleApiKeyLoading && (
-                    <span className='text-yellow-500 text-xs'>
-                      Validating Google API Key...
-                    </span>
-                  )}
-                  {!isGoogleApiKeyLoading && isGoogleApiKeyValid && (
-                    <span className='text-green-500 text-xs'>
-                      Google API Key is valid.
-                    </span>
-                  )}
-                  {!isGoogleApiKeyLoading && isGoogleApiKeyFailed && (
-                    <span className='text-red-500 text-xs'>
-                      Google API Key is invalid.
-                    </span>
-                  )}
-                </div>
-                <div className='flex gap-1'>
-                  <PasswordInput
-                    id='google-api-key'
-                    placeholder=''
-                    value={googleApiKey}
-                    onChange={onChangeGoogleApiKey}
-                    showPassword={false}
-                    autoFocus={false}
-                  />
-                  <Button
-                    variant='ghost'
-                    size='icon'
-                    onClick={handleValidateAndSaveGoogle}
-                    disabled={isGoogleApiKeyLoading}
-                  >
-                    {isGoogleApiKeyLoading ? (
-                      <Loader2 className='animate-spin' />
-                    ) : (
-                      <Plus />
-                    )}
-                  </Button>
-                </div>
+                {LLMProviderNameSchema.options.map(providerName => {
+                  const { isLoading, isValid } = statusMap[providerName];
+                  const error = formState.errors[providerName];
+
+                  return (
+                    <div key={providerName} className='flex flex-col gap-2'>
+                      <div className='flex flex-row gap-2'>
+                        <Label htmlFor={`${providerName}-api-key`}>
+                          {getProviderLogo(
+                            providerName as LLMProviderName,
+                            'size-4',
+                          )}
+                          {` ${providerName}`}
+                        </Label>
+                        {isLoading && (
+                          <span className='text-yellow-500 text-xs'>
+                            {`Validating ${providerName} API Key...`}
+                          </span>
+                        )}
+                        {!isLoading && isValid && (
+                          <span className='text-green-500 text-xs'>
+                            {`${providerName} API Key is valid.`}
+                          </span>
+                        )}
+                      </div>
+                      <div className='flex flex-col gap-1'>
+                        <div className='flex gap-1'>
+                          <PasswordInput
+                            {...register(providerName)}
+                            id={`${providerName}-api-key`}
+                            placeholder=''
+                            autoFocus={false}
+                          />
+                          <Button
+                            variant='ghost'
+                            size='icon'
+                            onClick={handleSubmit(
+                              handleValidateAndSave(
+                                providerName as LLMProviderName,
+                              ),
+                            )}
+                            disabled={isLoading}
+                          >
+                            {isLoading ? (
+                              <Loader2 className='animate-spin' />
+                            ) : (
+                              <Plus />
+                            )}
+                          </Button>
+                        </div>
+                        {error?.message && (
+                          <span className='text-red-500 text-xs'>
+                            {error.message}
+                          </span>
+                        )}
+                        <CardDescription className='text-xs mt-1'>
+                          {`Your API key is stored in your browser's local storage.`}
+                        </CardDescription>
+                      </div>
+                    </div>
+                  );
+                })}
               </div>
             </div>
           </CardContent>
-          <CardFooter>{/* <Button></Button> */}</CardFooter>
         </Card>
       </DialogContent>
     </Dialog>
