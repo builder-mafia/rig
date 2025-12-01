39e1112683f6c91dd98615e65c2eb0f246d9fe2a
gaki2 <qudrkr0815@gmail.com>
2025-12-01 14:20:33 +0900

    feat: LeftPanel ChatSelect

--- apps/web/src/app/components/left-panel/LeftPanel.tsx
+++ apps/web/src/app/components/left-panel/LeftPanel.tsx
@@ -1,13 +1,58 @@
-import { motion } from 'motion/react';
+import { useSetAtom } from 'jotai';
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from '@/components/ui/sidebar';
+import { useSwrAtomValue } from '@/hooks/use-swr-atom-value';
+import { dbAtoms } from '@/idb/db-store';
+import { assertDefined } from '@/utils/assert';
 
 export const LeftPanel = () => {
+  const allChannels = useSwrAtomValue(dbAtoms.allChannelsAtom);
+  const selectedChannelId = useSwrAtomValue(dbAtoms.selectedChannelIdAtom);
+  const setSelectedChannelId = useSetAtom(dbAtoms.selectedChannelIdAtom);
+
+  assertDefined(allChannels, 'LeftPanel: allChannels is not found.');
+  assertDefined(selectedChannelId, 'LeftPanel: selectedChannel is not found.');
+
+  const onClick = (channelId: string) => {
+    setSelectedChannelId(channelId);
+  };
+
   return (
-    <motion.div
-      initial={{ width: 0 }}
-      animate={{ width: 360 }}
-      exit={{ width: 0 }}
-      transition={{ ease: 'easeInOut', duration: 0.12 }}
-      className='h-full overflow-hidden shadow-md rounded-2xl bg-accent'
-    />
+    <Sidebar>
+      <SidebarHeader>
+        <div className='w-full h-[24px]'></div>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>My Chats</SidebarGroupLabel>
+          <SidebarGroupContent>
+            {allChannels.map(channel => {
+              return (
+                <SidebarMenuItem key={channel.id}>
+                  <SidebarMenuButton
+                    isActive={selectedChannelId === channel.id}
+                    onClick={() => onClick(channel.id)}
+                  >
+                    <span className='truncate'>
+                      {channel.name ?? channel.id}
+                    </span>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              );
+            })}
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter></SidebarFooter>
+    </Sidebar>
   );
 };
