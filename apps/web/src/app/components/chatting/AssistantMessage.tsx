ec0e175510d99f4c4ad3f4180ab5ea85501cb9aa
gaki2 <qudrkr0815@gmail.com>
2025-12-08 01:53:27 +0900

    feat: loading

--- apps/web/src/app/components/chatting/AssistantMessage.tsx
+++ apps/web/src/app/components/chatting/AssistantMessage.tsx
@@ -1,24 +1,65 @@
+import type { useChat } from '@ai-sdk/react';
 import type { UIMessage } from 'ai';
+import { AnimatePresence, motion } from 'motion/react';
+import { Spinner } from '@/components/ui/spinner';
 import { getAssistantMessageText } from '@/core/chat/message-util';
 import { assert } from '@/utils/assert';
+import { cn } from '@/utils/cn';
 import { Markdown } from './Markdown';
 
 type AssistantMessageProps = {
   message: UIMessage;
+  isLast: boolean;
+  status: ReturnType<typeof useChat>['status'];
 };
 
-export const AssistantMessage = ({ message }: AssistantMessageProps) => {
+export const AssistantMessage = ({
+  message,
+  isLast,
+  status,
+}: AssistantMessageProps) => {
   assert(
     message.role === 'assistant',
     `AssistantMessage: message is not a assistant message. message: ${JSON.stringify(message)}`,
   );
 
   const textMessage = getAssistantMessageText(message);
+  const isGenerating = status === 'streaming';
 
   return (
     // override the default prose max-width to none.
-    <article className='prose dark:prose-invert max-w-none'>
+    <article className='prose dark:prose-invert max-w-none relative'>
       <Markdown messageId={message.id} text={textMessage} />
+      <AnimatePresence>
+        {isLast && isGenerating && (
+          <motion.div
+            initial={{
+              opacity: 0,
+              filter: 'blur(2px)',
+            }}
+            animate={{
+              opacity: 1,
+              filter: 'blur(0px)',
+            }}
+            exit={{
+              opacity: 0,
+              filter: 'blur(2px)',
+            }}
+            transition={{
+              duration: 0.17,
+              ease: 'easeInOut',
+            }}
+            className={cn(
+              'absolute flex items-center justify-center text-sm text-muted-foreground gap-1.5 w-full -top-6',
+            )}
+          >
+            <Spinner size='xs' variant='default' className='opacity-50' />
+            <p className='text-xs !m-0 text-muted-foreground opacity-75'>
+              generating...
+            </p>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </article>
   );
 };
