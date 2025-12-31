95b9002bb2de744c43637a3f176e2c988dde686e
gaki2 <qudrkr0815@gmail.com>
2025-12-31 21:03:42 +0900

    feat: remove tauri desktop app

--- apps/desktop-app/src-tauri/src/main.rs
+++
@@ -1,8 +0,0 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
-fn main() {
-    app_lib::run();
-}
-
-// sqlite3 Library/Application\ Support/com.tauri.dev/ALLIN.sqlite
