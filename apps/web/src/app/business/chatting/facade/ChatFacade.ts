6fe1c5fb1408c5f434ce2251b3c3ca08862b95bc
gaki2 <qudrkr0815@gmail.com>
2026-01-10 18:17:38 +0900

    refactor: change method name more explicitly

--- apps/web/src/app/business/chatting/facade/ChatFacade.ts
+++ apps/web/src/app/business/chatting/facade/ChatFacade.ts
@@ -154,7 +154,33 @@ export class ChatFacade {
     this.uiMessageStore.setUiMessages(setter);
   }
 
-  public upsertUiMessage(message: ChatUiMessage) {
+  /**
+   * replace the message in the context
+   */
+  public replaceMessage(message: ChatUiMessage) {
+    const existingMessageIndex = this.getUiMessages().findIndex(
+      m => m.id === message.id,
+    );
+
+    if (existingMessageIndex === -1) {
+      throw new Error(
+        `replaceMessage: target message not found: ${message.id}.`,
+      );
+    }
+
+    const newMessages = [
+      ...this.getUiMessages().slice(0, existingMessageIndex),
+      message,
+      ...this.getUiMessages().slice(existingMessageIndex + 1),
+    ];
+
+    this.setUiMessages(newMessages);
+  }
+
+  /**
+   * create or replace the message in the context
+   */
+  public createOrReplaceMessage(message: ChatUiMessage) {
     const existingMessageIndex = this.getUiMessages().findIndex(
       m => m.id === message.id,
     );
@@ -175,16 +201,11 @@ export class ChatFacade {
     this.chat.messages = [...this.chat.messages, message];
   }
 
-  public replaceContextMessage(message: ChatUiMessage) {
-    const targetMessage = this.chat.messages.find(msg => msg.id === message.id);
-
-    if (targetMessage) {
-      this.chat.messages = this.chat.messages.map(msg =>
-        msg.id === message.id ? message : msg,
-      );
-    } else {
-      throw new Error(`target context message not found: ${message.id}.`);
-    }
+  /**
+   * check if the message is in the context
+   */
+  public hasMessage(messageId: string) {
+    return this.chat.messages.some(msg => msg.id === messageId);
   }
 
   public async sendMessage(message: ChatUiMessage & { role: 'user' }) {
@@ -259,7 +280,7 @@ export class ChatFacade {
     chat['~registerMessagesCallback'](() => {
       const streamingMessage = chat.lastMessage;
       if (streamingMessage) {
-        this.upsertUiMessage(streamingMessage);
+        this.createOrReplaceMessage(streamingMessage);
       }
     }, throttleTime);
 
