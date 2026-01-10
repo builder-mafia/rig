9a4f435965213a06f1889e277eca9bb116a8c9e7
gaki2 <qudrkr0815@gmail.com>
2026-01-10 15:12:54 +0900

    fix: change Skeleton UI

--- apps/web/src/app/business/chatting/ChattingSuspenseFallbackView.tsx
+++ apps/web/src/app/business/chatting/ChattingSuspenseFallbackView.tsx
@@ -1,12 +1,5 @@
-import { Skeleton } from '@allin/ui';
-
 export const ChattingSuspenseFallbackView = () => {
   return (
-    <div className='w-full h-full flex flex-col gap-6 p-20 max-w-2xl lg:max-w-4xl mx-auto overflow-y-hidden'>
-      <Skeleton className='opacity-40 w-full h-8 self-end shrink-0' />
-      <Skeleton className='opacity-40 w-full h-100 shrink-0' />
-      <Skeleton className='opacity-40 w-full h-8 self-end shrink-0 mt-16' />
-      <Skeleton className='opacity-40 w-full h-200 shrink-0' />
-    </div>
+    <div className='w-full h-full flex flex-col gap-6 p-20 max-w-2xl lg:max-w-4xl mx-auto overflow-y-hidden'></div>
   );
 };
