68d7eb7dac8c95c797cb8460d8a526ab13d55af2
gaki2 <qudrkr0815@gmail.com>
2026-02-12 00:18:08 +0900

    ui: textarea fontsize change

--- packages/ui/src/components/ui/textarea.tsx
+++ packages/ui/src/components/ui/textarea.tsx
@@ -16,7 +16,7 @@ function Textarea({ className, ...props }: React.ComponentProps<'textarea'>) {
     <textarea
       data-slot='textarea'
       className={cn(
-        'border-ring placeholder:text-muted-foreground selection:text-secondary selection:bg-secondary-foreground focus-visible:border-ring focus-visible:ring-ring/0 text-secondary-foreground aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 flex field-sizing-content min-h-16 w-full rounded-2xl border bg-background px-3 py-2 text-base shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[2px] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm resize-none max-h-24 dark:text-foreground lg:text-base caret-gray-300',
+        'border-ring placeholder:text-muted-foreground selection:text-secondary selection:bg-secondary-foreground focus-visible:border-ring focus-visible:ring-ring/0 text-secondary-foreground aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 flex field-sizing-content min-h-16 w-full rounded-2xl border bg-background px-3 py-2 text-base shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[2px] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm resize-none max-h-24 dark:text-foreground caret-gray-300',
         className,
       )}
       {...props}
