cb82a83da33d58b039472c834f92a8ac883d4f05
gaki2 <qudrkr0815@gmail.com>
2026-03-20 15:57:09 +0900

    fix: deprecate web application

--- apps/web/README.md
+++
@@ -1,3 +0,0 @@
-This app is deprecated. 
-
-We decided all-in to be a desktop app only. :)
--- apps/web/src/app/business/transport/index.ts
+++
@@ -1,6 +0,0 @@
-export {
-  createSpeech,
-  type SpeakOptions,
-  type SpeakResult,
-} from './create-speech';
-export { createTransport } from './create-transport';
